e0b38ea50997210d5cac273cadd6f53aeec9561b
Tarina Islam Tisha <khantisha889@gmail.com>
2026-07-30 11:16:03 +0600

    Add sign outbutton to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 const Navbar = () => {
+    const { auth } = usePuterStore();
+
     return (
         <nav className="navbar">
             <Link to="/">
                 <p className="text-2xl font-bold text-gradient">RESUMIND</p>
             </Link>
-            <Link to="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
+            <div className="flex flex-row items-center gap-4">
+                <Link to="/upload" className="primary-button w-fit">
+                    Upload Resume
+                </Link>
+                {auth.isAuthenticated && (
+                    <button
+                        onClick={() => auth.signOut()}
+                        className="text-sm text-gray-500 hover:text-gray-800 cursor-pointer"
+                    >
+                        Sign Out
+                    </button>
+                )}
+            </div>
         </nav>
     );
 };
